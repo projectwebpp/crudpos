0fdc1e10243e4e0561a5c07495f0e309f114a40f
projectwebpp <124865459+projectwebpp@users.noreply.github.com>
2026-03-17 09:57:30 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -2,11 +2,11 @@
 session_start();
 
 // ตั้งค่าฐานข้อมูล
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'crud_system');
-define('BASE_URL', 'http://localhost/crudphp');
+define('DB_HOST', 'sql12.freesqldatabase.com');
+define('DB_USER', 'sql12820323');
+define('DB_PASS', '3byAVLpJSr');
+define('DB_NAME', 'sql12820323');
+define('BASE_URL', 'http://sql12.freesqldatabase.com');
 
 // เชื่อมต่อฐานข้อมูล
 try {
